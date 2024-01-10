fc42749bca660ea94213f654890700640c0c99be
Connor Saila <csaila@live.com>
2024-01-10 16:41:57 -0500

    lock and logout added

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -65,15 +65,7 @@ run_cmd() {
 			amixer set Master mute
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
+			loginctl kill-user $(whoami)
 		fi
 	else
 		exit 0
@@ -90,11 +82,7 @@ case ${chosen} in
 		run_cmd --reboot
         ;;
     $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
+		swaylock -i ${HOME}/Downloads/momiwallpaper.png --indicator-idle-visible
         ;;
     $suspend)
 		run_cmd --suspend
