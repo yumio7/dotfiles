98534ca3ac3ef7d284537bc5fcf6b786a1be1e65
Connor Saila <csaila@live.com>
2024-01-10 13:35:25 -0500

    starship configured

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     cat ~/.cache/wal/sequences
     fish_ssh_agent
+    starship init fish | source
 end
+fish_add_path /home/yumio/.spicetify
