1f5a8ccd1f55b49ec9e156e9395c0989b6a6c52a
Connor Saila <csaila@live.com>
2024-02-25 14:52:24 -0500

    fix terminal colors and a splash that almost bricked my system

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    cat ~/.cache/wal/sequences
+    # cat ~/.cache/wal/sequences
     fish_ssh_agent
     starship init fish | source
 end
